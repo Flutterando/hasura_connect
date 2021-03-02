d498baf332e392e624a6ccab4ce2daaff8e478d1
Jacob Moura <jacobaraujo7@gmail.com>
2021-03-02 16:46:20 -0300

    update dependencies

---
+++ example/lib/app/app_module.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends MainModule {}
---
+++ example/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'app/app_module.dart';
+
+void main() {
+  runApp(ModularApp(module: AppModule()));
+}
