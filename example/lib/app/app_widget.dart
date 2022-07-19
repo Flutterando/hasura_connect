49f6bdc2d728bf0acbae546a70b4ef941fb9e2d2
Bwolf <bwolfnoob@gmail.com>
2022-07-19 13:21:24 -0300

    Fix Example

--- example/lib/app/app_widget.dart
+++ example/lib/app/app_widget.dart
@@ -4,11 +4,11 @@ import 'package:flutter_modular/flutter_modular.dart';
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-    ).modular();
+      theme: ThemeData(primarySwatch: Colors.blue),
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    );
   }
 }
